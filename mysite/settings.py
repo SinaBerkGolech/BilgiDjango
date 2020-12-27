796c5867ab8885cb4ca8f3efe64aec83c1de81ec
Sina Berk <49809794+SinaBerkGolech@users.noreply.github.com>
2020-12-27 21:47:44 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-nkr7^5igyatxu8*+4hiewsq5jk-s7059za4q_oqg0_trt839k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'myresourcegroupag.azurewebsites.net']
 
